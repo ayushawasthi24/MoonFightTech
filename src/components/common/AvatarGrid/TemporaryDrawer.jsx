9a31afed8f84859c654be7581627a45edbd3902d
shubham-ft <139536877+shubham-fantv@users.noreply.github.com>
2024-11-08 13:07:06 +0530

    small fix

--- src/components/common/AvatarGrid/TemporaryDrawer.jsx
+++ src/components/common/AvatarGrid/TemporaryDrawer.jsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import AvatarGrid from './AvatarGrid';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import AvatarGrid from "./AvatarGrid";
 
 export default function TemporaryDrawer() {
   const [open, setOpen] = React.useState(false);
@@ -13,8 +13,8 @@ export default function TemporaryDrawer() {
 
   return (
     <div>
-      <Button onClick={toggleDrawer(true)}>Open Avatar Grid</Button> {/* Button to open the drawer */}
-      
+      <Button onClick={toggleDrawer(true)}>Open Avatar Grid</Button>{" "}
+      {/* Button to open the drawer */}
       <Drawer open={open} onClose={toggleDrawer(false)}>
         <Box sx={{ width: 390 }} role="presentation" onClick={toggleDrawer(false)}>
           {/* Render AvatarGrid inside the drawer */}
@@ -24,4 +24,3 @@ export default function TemporaryDrawer() {
     </div>
   );
 }
-

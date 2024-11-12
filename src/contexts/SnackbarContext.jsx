fa02acc51b0bca6cf4a98a0dd7046cf2412338d4
shubham-ft <139536877+shubham-fantv@users.noreply.github.com>
2024-11-12 16:54:53 +0530

    snackbar position changed

--- src/contexts/SnackbarContext.jsx
+++ src/contexts/SnackbarContext.jsx
@@ -13,12 +13,9 @@ export const SnackbarProvider = ({ children }) => {
     duration: 3000,
   });
 
-  const showSnackbar = useCallback(
-    (message, type = "info", duration = 3000) => {
-      setSnackbar({ open: true, message, type, duration });
-    },
-    []
-  );
+  const showSnackbar = useCallback((message, type = "info", duration = 3000) => {
+    setSnackbar({ open: true, message, type, duration });
+  }, []);
 
   const handleClose = () => {
     setSnackbar((prev) => ({ ...prev, open: false }));
@@ -31,13 +28,9 @@ export const SnackbarProvider = ({ children }) => {
         open={snackbar.open}
         autoHideDuration={snackbar.duration}
         onClose={handleClose}
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
       >
-        <Alert
-          onClose={handleClose}
-          severity={snackbar.type}
-          sx={{ width: "100%" }}
-        >
+        <Alert onClose={handleClose} severity={snackbar.type} sx={{ width: "100%" }}>
           {snackbar.message}
         </Alert>
       </Snackbar>
